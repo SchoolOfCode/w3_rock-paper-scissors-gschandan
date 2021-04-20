6111acba30894a1af872f7032f1a0a2bcdc1e5d8
bit-modulator <neurapraxia@gmail.com>
2021-04-20 18:56:06 +0100

    initial game logic setup, todo: loop, then add styling, play in browser not console.

---
+++ main.js
@@ -0,0 +1,48 @@
+let player = {
+    name: "",
+    move: 0,
+    disp_move: "",
+    wins: 0,
+    draws: 0,
+    losses: 0,
+    games: 0,
+    results: function(){
+        return `${this.name}'s results over ${this.games} game(s) are: ${this.wins} Wins , ${this.draws} Draws and ${this.losses} Losses.`;
+    }
+};
+let computer ={
+    move: 0,
+    disp_move: ""
+};
+let options = {  
+    rock: 1,
+    paper: 2,
+    scissors: 3,
+    disp: ["","rock","paper","scissors"]
+};
+let outcomes = ["won", "lost", "drew"];
+
+player.name=prompt("Please enter your name:");
+player.move=options[prompt("Rock, scissors or paper?:").toLowerCase()];
+player.disp_move = options.disp[player.move]; //convert integer representation of move to a string
+computer.move=Math.floor(Math.random()*2)+1;  //random number between 1 and 3
+computer.disp_move = options.disp[computer.move];
+
+let game=win_check();
+console.log(`${player.name} played ${player.disp_move} against ${computer.disp_move}, and ${game}.`);
+console.log(player.results());
+
+function win_check(){
+    player.games++;
+    //1 beats 3, 2 beats 1, and 3 beats 2
+    if (player.move === computer.move){
+        player.draws++;
+        return outcomes[2];
+    } else if ((player.move - computer.move) % 3 == 1){
+        player.wins++;
+        return outcomes[0];
+    } else{
+        player.losses++;
+        return outcomes[1];
+    }
+}
