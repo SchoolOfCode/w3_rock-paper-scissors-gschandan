892709e2214ccba3a95d24d908e6235cf0ff84cc
bit-modulator <neurapraxia@gmail.com>
2021-04-20 23:36:25 +0100

    fixed bug in selecting, added greying out of rejected options

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 let player = {
     name: "",
     move: 0,
-    disp_move: "",
     wins: 0,
     draws: 0,
     losses: 0,
@@ -12,24 +11,40 @@ let player = {
 };
 let computer ={
     move: 0,
-    disp_move: ""
-};
-let options = {  
-    rock: 1,
-    paper: 2,
-    scissors: 3,
-    disp: ["","rock","paper","scissors"]
 };
 let outcomes = ["won", "lost", "drew"];
 
-player.name=prompt("Please enter your name:");
-player.move=options[prompt("Rock, scissors or paper?:").toLowerCase()];
-player.disp_move = options.disp[player.move];      //convert integer representation of move to a string
+function set_player_move(move_clicked){
+    let icons = ["icon-rock","icon-paper","icon-scissors"];
+    let chosen_move = icons.splice(move_clicked-1,1);
+    for(i=0;i<2;i++){
+        document.getElementById(icons[i]).style.opacity = "0.2";
+    }
+    icons.push(chosen_move);
+    document.getElementById(chosen_move).style.color = "rgb(128, 240, 162)";
+
+    player.move=move_clicked;
+    console.log(player.move,computer.move);
+    
+    //reset(icons);
+}
 computer.move=Math.floor(Math.random()*2)+1;      //random number between 1 and 3
-computer.disp_move = options.disp[computer.move];  //convert integer representation of move to a string
-let game=win_check();
-console.log(`${player.name} played ${player.disp_move} against ${computer.disp_move}, and ${game}.`);
-console.log(player.results());
+
+function reset(icons){
+    for(i=0;i<3;i++){
+        document.getElementById(icons[i]).style.color = "";
+        document.getElementById(icons[i]).style.opacity = "";
+    }
+}
+
+//player.name=prompt("Please enter your name:");
+//document.getElementById("name").innerHTML = player.name;
+//player.move=options[prompt("Rock, scissors or paper?:").toLowerCase()];
+
+//convert integer representation of move to a string
+//let game=win_check();
+//console.log(`${player.name} played ${player.disp_move} against ${computer.disp_move}, and ${game}.`);
+//console.log(player.results());
 
 
 function win_check(){
