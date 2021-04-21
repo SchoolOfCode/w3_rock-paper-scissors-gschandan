9979e90d35b6f40f25aee9322a68483351a5d480
bit-modulator <neurapraxia@gmail.com>
2021-04-21 13:07:29 +0100

    barebones JS now working with icons TODO: add username and recall function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let player = {
     name: "",
-    move: 0,
+    move: null,
     wins: 0,
     draws: 0,
     losses: 0,
@@ -10,10 +10,12 @@ let player = {
     }
 };
 let computer ={
-    move: 0,
+    move: null,
 };
 let outcomes = ["won", "lost", "drew"];
 
+//player.name=prompt("Please enter your name:");
+//document.getElementById("name").innerHTML = player.name;
 function set_player_move(move_clicked){
     let icons = ["icon-rock","icon-paper","icon-scissors"];
     let chosen_move = icons.splice(move_clicked-1,1);
@@ -21,41 +23,39 @@ function set_player_move(move_clicked){
         document.getElementById(icons[i]).style.opacity = "0.2";
     }
     icons.push(chosen_move);
-    document.getElementById(chosen_move).style.color = "rgb(128, 240, 162)";
-    document.getElementById(chosen_move).style.color = "rgb(128, 240, 162)";
-    document.getElementById(chosen_move).style.color = "rgb(128, 240, 162)";
-    
+    document.getElementById(chosen_move).style.color = "rgb(154, 206, 167)";
     player.move=move_clicked;
-    console.log(player.move,computer.move);
-    
-    //reset(icons);
 }
-computer.move=Math.floor(Math.random()*2)+1;      //random number between 1 and 3
 
-function reset(icons){
-    for(i=0;i<3;i++){
-        document.getElementById(icons[i]).style.color = "";
-        document.getElementById(icons[i]).style.opacity = "";
+function set_computer_move(){
+    computer.move=Math.floor(Math.random()*2)+1;
+}
+
+function play_game(){
+    set_computer_move();
+    if (player.move !== null){
+        let result = win_check();
+        console.log(result, player.results());
     }
+    reset_icons();
 }
 
-//player.name=prompt("Please enter your name:");
-//document.getElementById("name").innerHTML = player.name;
-//player.move=options[prompt("Rock, scissors or paper?:").toLowerCase()];
-
-//convert integer representation of move to a string
-//let game=win_check();
-//console.log(`${player.name} played ${player.disp_move} against ${computer.disp_move}, and ${game}.`);
-//console.log(player.results());
-
+function reset_icons(){
+    let icons_reset = ["icon-rock","icon-paper","icon-scissors"];
+    for(i=0;i<3;i++){
+        document.getElementById(icons_reset[i]).style.color = "";
+        document.getElementById(icons_reset[i]).style.opacity = "";
+    }
+    player.move=null;
+    computer.move=null;
+}
 
 function win_check(){
     player.games++;
-    //1 beats 3, 2 beats 1, and 3 beats 2
     if (player.move === computer.move){
         player.draws++;
         return outcomes[2];
-    } else if ((player.move - computer.move) % 3 == 1){
+    } else if ((player.move - computer.move) % 3 == 1){    //1 beats 3, 2 beats 1, and 3 beats 2
         player.wins++;
         return outcomes[0];
     } else{
